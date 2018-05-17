31ccb904e1ef5b886558deb294c4685a7551c9f1
Field User <field@ccom.unh.edu>
2018-05-17 14:57:21 -0400

    Implemented two zones for obstacle detection.

--- nodes/radar_obstacle_detection_node.py
+++ nodes/radar_obstacle_detection_node.py
@@ -8,15 +8,18 @@ radar_buffer = {}
 range_m = 0.0
 obstacle_publisher = None
 
+short_range_cutoff = 25 # distance where short range ends
+short_range_angle = 20 # angle to look for obstacles between 0 and short_range_cutoff
+long_range_angle = 10 # angle to look for obstacles beyond short_range_cutoff
+
+short_range_angle_range = (360-(short_range_angle/2),short_range_angle/2)
+long_range_angle_range = (360-(long_range_angle/2),long_range_angle/2)
+
 def radar_callback(data):
     global radar_buffer
     global range_m
     for s in data.sector.scanlines:
-        if s.angle > 355 or s.angle < 5:
-	    #print s.angle, s.range,
-            #for i in range(2,100):
-            #    print ord(s.intensities[i]),
-            #print
+        if s.angle > short_range_angle_range[0] or s.angle < short_range_angle_range[1]:
             radar_buffer[s.angle] = s
             range_m = s.range
     if len(radar_buffer):
@@ -24,11 +27,14 @@ def radar_callback(data):
         for i in range(len(radar_buffer[radar_buffer.keys()[0]].intensities)):
             avg_intensities.append(0)
         #print radar_buffer
+        bin_size =  range_m/float(len(avg_intensities))
         for s in radar_buffer.itervalues():
             for i in range(4,len(s.intensities)):
-                avg_intensities[i] += (ord(s.intensities[i])/255.0)/float(len(radar_buffer))
+                if i*bin_size > short_range_cutoff and (s.angle > long_range_angle_range[0] or s.angle < long_range_angle_range[1]):
+                    avg_intensities[i] += (ord(s.intensities[i])/255.0)/float(len(radar_buffer))*short_range_angle/long_range_angle                                        
+                else:
+                    avg_intensities[i] += (ord(s.intensities[i])/255.0)/float(len(radar_buffer))
         #print range_m, range_m/float(len(avg_intensities)) ,  avg_intensities[0:50]
-        bin_size =  range_m/float(len(avg_intensities))
         nearest = None
         for i in range(len(avg_intensities)):
             if avg_intensities[i] > .15:
